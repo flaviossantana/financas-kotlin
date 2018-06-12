4df3413b3267eeadfea9a10eecc759241d91ccb9
flavio.santana <9090flavio@gmail.com>
2018-06-11 22:30:11 -0300

    Implementando o próprio Adapter - Parte 1

---
+++ app/src/main/java/br/com/alura/financas/ui/ListatTrancasoesAdapter.kt
@@ -0,0 +1,24 @@
+package br.com.alura.financas.ui
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+
+class ListatTrancasoesAdapter : BaseAdapter() {
+
+    override fun getView(posicao: Int, convertView: View?, parent: ViewGroup?): View {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItem(posicao: Int): Any {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemId(posicao: Int): Long {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
