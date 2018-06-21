8ce0998e2dcbb6fe94cd648553a3ef35a8f93b5b
flavio.santana <9090flavio@gmail.com>
2018-06-20 22:07:52 -0300

    Aplicando o total no resumo

--- app/src/main/java/br/com/alura/financas/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/alura/financas/ui/activity/ListaTransacoesActivity.kt
@@ -2,12 +2,14 @@ package br.com.alura.financas.ui.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import br.com.alura.financas.R
-import br.com.alura.financas.adapter.ListatTrancasoesAdapter
+import br.com.alura.financas.ui.adapter.ListatTrancasoesAdapter
 import br.com.alura.financas.enum.Tipo.DESPESA
 import br.com.alura.financas.enum.Tipo.RECEITA
 import br.com.alura.financas.extension.formatarReal
 import br.com.alura.financas.model.Transacao
+import br.com.alura.financas.ui.ResumoView
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 import kotlinx.android.synthetic.main.resumo_card.*
 import java.math.BigDecimal
@@ -23,23 +25,16 @@ class ListaTransacoesActivity : AppCompatActivity() {
                                 Transacao(valor = BigDecimal(100.0), categoria = "ECONOMIA", tipo = RECEITA),
                                 Transacao(valor = BigDecimal(99.40), tipo = DESPESA))
 
-        var totalReceita = BigDecimal.ZERO
-        var totalDespesa = BigDecimal.ZERO
-
-        for (transacao in transacoes){
-            if(transacao.isReceita()){
-                totalReceita = totalReceita.plus(transacao.valor)
-            }
-            if(transacao.isDespesa()){
-                totalDespesa = totalDespesa.plus(transacao.valor)
-            }
-        }
-
-        resumo_card_receita.setText(totalReceita.formatarReal())
-        resumo_card_despesa.setText(totalDespesa.formatarReal())
-        resumo_card_total.setText(totalReceita.subtract(totalDespesa).formatarReal())
+        configResumo(transacoes)
 
         lista_transacoes_listview.adapter = ListatTrancasoesAdapter(transacoes, this)
+    }
+
+    private fun configResumo(transacoes: List<Transacao>) {
+        val resumoView = ResumoView(window.decorView, transacoes)
+        resumoView.addDespesa()
+        resumoView.addReceita()
+        resumoView.calcularTotal()
     }
 
 
