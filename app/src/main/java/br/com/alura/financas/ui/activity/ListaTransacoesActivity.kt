e52ed14cb808668c05ad8205891a5c6dcea8a92f
flavio.santana <9090flavio@gmail.com>
2018-06-27 22:32:29 -0300

    Implementando o DatePickerDialog

--- app/src/main/java/br/com/alura/financas/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/alura/financas/ui/activity/ListaTransacoesActivity.kt
@@ -8,16 +8,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import br.com.alura.financas.R
-import br.com.alura.financas.ui.adapter.ListatTrancasoesAdapter
 import br.com.alura.financas.enum.Tipo.DESPESA
 import br.com.alura.financas.enum.Tipo.RECEITA
 import br.com.alura.financas.extension.diaMesAno
-import br.com.alura.financas.extension.formatarReal
 import br.com.alura.financas.model.Transacao
 import br.com.alura.financas.ui.ResumoView
+import br.com.alura.financas.ui.adapter.ListatTrancasoesAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 import kotlinx.android.synthetic.main.form_transacao.view.*
-import kotlinx.android.synthetic.main.resumo_card.*
 import java.math.BigDecimal
 import java.util.*
 
