8e21a058d931ed81541eb9bab59bed1f55438bab
flavio.santana <9090flavio@gmail.com>
2018-06-27 22:17:00 -0300

    Inserindo layout no Dialog

--- app/src/main/java/br/com/alura/financas/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/alura/financas/ui/activity/ListaTransacoesActivity.kt
@@ -1,8 +1,11 @@
 package br.com.alura.financas.ui.activity
 
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import br.com.alura.financas.R
 import br.com.alura.financas.ui.adapter.ListatTrancasoesAdapter
@@ -23,21 +26,33 @@ class ListaTransacoesActivity : AppCompatActivity() {
         setContentView(R.layout.activity_lista_transacoes)
 
         val transacoes = listOf(Transacao(valor = BigDecimal(20.50), categoria = "COMIDA", tipo = DESPESA),
-                                Transacao(valor = BigDecimal(100.0), categoria = "ECONOMIA", tipo = RECEITA),
-                                Transacao(valor = BigDecimal(99.40), tipo = DESPESA))
+                Transacao(valor = BigDecimal(100.0), categoria = "ECONOMIA", tipo = RECEITA),
+                Transacao(valor = BigDecimal(99.40), tipo = DESPESA))
 
         configResumo(transacoes)
 
         lista_transacoes_listview.adapter = ListatTrancasoesAdapter(transacoes, this)
 
-        lista_transacoes_adiciona_receita.setOnClickListener(View.OnClickListener {
-            Toast.makeText(this, "Adicionou Receita!", Toast.LENGTH_LONG).show()
-        })
+        addReceita()
 
         lista_transacoes_adiciona_despesa.setOnClickListener(View.OnClickListener {
             Toast.makeText(this, "Adicionou Despesa!", Toast.LENGTH_LONG).show()
         })
 
+    }
+
+    private fun addReceita() {
+        lista_transacoes_adiciona_receita.setOnClickListener(View.OnClickListener {
+            Toast.makeText(this, "Adicionou Receita!", Toast.LENGTH_LONG).show()
+
+            val view = LayoutInflater.from(this).inflate(R.layout.form_transacao, it.parent as ViewGroup, false)
+
+            AlertDialog.Builder(this)
+                    .setTitle(R.string.adiciona_receita)
+                    .setView(view)
+                    .show()
+
+        })
     }
 
     private fun configResumo(transacoes: List<Transacao>) {
